ba0adc0404759e16e8997f8fedf256c3df375c44
Rahul Parmar <rahulparmar.n@gmail.com>
2020-03-20 12:35:31 +0530

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -2,5 +2,5 @@
 
 sh -c "echo Hello world my name is $INPUT_MY_NAME"
 export TEST_FROM_SHELL="TEST_FROM_SHELL"
-sh -c "echo world my name is $TEST_IN_SHELL"
+sh -c "echo world my name is ${{ env.TEST_IN_SHELL }}"
 
